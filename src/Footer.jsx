21714119867a939d9c9a19e833b8b7391d488dd1
mrxando <73470507+mrxando@users.noreply.github.com>
2023-09-07 17:12:30 +0200

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -7,10 +7,10 @@ export default function Footer() {
             <a href="https://twitter.com/xvndoo" target="_blank">
                 <img src="Twitter Icon.svg" alt="Twitter Icon" />
             </a>
-            <a href="https://www.facebook.com/mrxando/" target="_blank" >
+            <a href="https://www.facebook.com/" target="_blank" >
                 <img src="Facebook Icon.svg" alt="Facebook Icon" />
             </a>
-            <a href="https://www.instagram.com/xvndoo_/" target="_blank">
+            <a href="https://www.instagram.com//" target="_blank">
                 <img src="Instagram Icon.png" alt="Instagram Icon" />
             </a>
             <a href="https://github.com/mrxando" target="_blank">
